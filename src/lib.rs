acd5e098b655805db77257b9c1ce18ba00b6131d
Mairo Rufus <akoudanilo@gmail.com>
2021-05-30 21:22:02 +0100

    Implement Matrix sum

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,12 @@ mod tests {
 
     #[test]
     fn matrix_creation() {
-        let m = matrix::Matrix::new(3, 3);
+        let m = matrix::Matrix::new(3, 3, 0.0);
         assert_eq!(m.data, vec![vec![0.0f64; 3]; 3]);
+    }
+
+    #[test]
+    fn matrix_sum() {
+        assert_eq!((matrix::Matrix::new(3, 3, 2.0) + matrix::Matrix::new(3, 3, 1.0)).data, vec![vec![3.0; 3]; 3])
     }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 pub struct Matrix {
     n: usize,
     m: usize,
@@ -5,12 +7,29 @@ pub struct Matrix {
 }
 
 impl Matrix {
-    pub fn new(n: usize, m: usize) -> Matrix {
+    pub fn new(n: usize, m: usize, e: f64) -> Matrix {
         let matrix = Matrix {
             n: n,
             m: m,
-            data: vec![vec![0.0f64; n]; m],
+            data: vec![vec![e; n]; m],
         };
         return matrix;
+    }
+}
+impl ops::Add<Matrix> for Matrix {
+    type Output = Self;
+
+    fn add(self, rhs: Matrix) -> Matrix {
+        if self.n != rhs.n || self.m != rhs.m {
+            panic!("Incompatible sizes");
+        }
+
+        let mut matrix_sum = Matrix::new(self.n, self.m, 0.0);
+        for i in 0..self.n {
+            for j in 0..self.m {
+                matrix_sum.data[i][j] = self.data[i][j] + rhs.data[i][j]
+            }
+        }
+        return matrix_sum;
     }
 }
